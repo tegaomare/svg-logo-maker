216c29ec0c32e70339793498e99474724f3d186f
Tegaomare <tegaomare@tegas-imac.mynetworksettings.com>
2023-06-12 22:36:27 -0400

    updated index.js

--- index.js
+++ index.js
@@ -1,62 +1,44 @@
-//Including packages etc needed for this application
-const inquirer = require("inquirer");
-const fs = require("fs");
-const generateLogo = require("./utils/generateLogo.js");
+const { prompt } = require("inquirer");
+const generateSvg = require("./lib/utils");
 
-//array of questions for user input
 const questions = [
   {
+    type: "input",
     name: "text",
-    type: "input",
-    message: "Enter up to three characters for text:",
+    message: "Text to be entered in the logo (Max of 3 Letters)",
+    default: "SVG",
+    validate(input) {
+      if (input.length > 3) return "Text can only be a max of 3 Characters";
+      if (input.trim() === "") return "Must include text";
+      return true;
+    },
+    filter(input) {
+      return input.toUpperCase();
+    },
   },
   {
+    type: "input",
     name: "textColor",
-    type: "input",
-    message: "Input text color (Enter color keyword OR a hexadecimal number):",
+    message: "Color of Text: (color keyword or hexadecimal number)",
+    default: "white",
   },
   {
-    name: "shape",
     type: "list",
-    message: "Select shape:",
-    choices: ["Circle", "Triangle", "Square"],
+    name: "shape",
+    message: "Which shape would you like this SVG to be?",
+    choices: ["Circle", "Square", "Triangle"],
+    default: 0,
   },
   {
-    name: "shapeColor",
     type: "input",
-    message: "Input shape color (Enter color keyword OR a hexadecimal number):",
+    name: "shapeColor",
+    message: "Color of Shape: (color keyword or hexadecimal number)",
+    default: "blue",
   },
 ];
 
-//function to write file
-function writeToFile(fileName, data) {
-  fs.writeFile(fileName, data, (err) => {
-    if (err) throw err;
-    console.log("Generated logo.svg");
+(() => {
+  prompt(questions).then((answers) => {
+    generateSvg(answers);
   });
-}
-
-//function to handleAnswers
-const handleAnswers = (answers) => {
-  if (answers.text.length > 3) {
-    console.log("Text must be 3 characters or less");
-    init(); //makes user enter params again if text if more than 3 chars
-  } else {
-    //Generate SVG file
-    //console.log(answers);
-    writeToFile("logo.svg", generateLogo(answers));
-  }
-};
-// WHEN I have entered input for all the prompts
-// THEN an SVG file is created named `logo.svg`
-// AND the output text "Generated logo.svg" is printed in the command line
-// WHEN I open the `logo.svg` file in a browser
-// THEN I am shown a 300x200 pixel image that matches the criteria I entered
-
-//Creating a function to initialize app
-function init() {
-  inquirer.prompt(questions).then(handleAnswers);
-}
-
-// Function call to initialize app
-init();
+})();
